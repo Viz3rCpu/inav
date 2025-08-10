d15d1274a0c79b0ca30786f7f2309d54a928f1a1
Viz3rCpu <59398582+Viz3rCpu@users.noreply.github.com>
2025-08-10 13:12:21 +0300

    Update target.h

--- src/main/target/SPEEDYBEEF405WING/target.h
+++ src/main/target/SPEEDYBEEF405WING/target.h
@@ -1,4 +1,3 @@
-@ -1,20 +1,3 @@
 /*
  * This file is part of INAV.
  *
@@ -19,7 +18,16 @@
 #pragma once
 
 #define TARGET_BOARD_IDENTIFIER "SP4W"
-@@ -31,29 +14,19 @@
+#define USBD_PRODUCT_STRING  "SpeedyBee F405 Wing"
+
+// LEDs
+#define LED0                    PA14  //Blue
+#define LED1                    PA13  //Green
+
+// Beeper
+#define BEEPER                  PC15
+#define BEEPER_INVERTED
+
 /*
  * Buses
  */
@@ -36,16 +44,12 @@
 #define SPI2_SCK_PIN            PB13
 #define SPI2_MISO_PIN   	    PC2
 #define SPI2_MOSI_PIN   	    PC3
-#define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PA7
 
 // SPI3
 #define USE_SPI_DEVICE_3
 #define SPI3_SCK_PIN            PB3
 #define SPI3_MISO_PIN   	    PB4
 #define SPI3_MOSI_PIN   	    PB5
-#define SPI3_MISO_PIN           PB4
-#define SPI3_MOSI_PIN           PB5
 
 // Serial ports
 #define USE_VCP
@@ -53,7 +57,8 @@
 #define USE_UART1
 #define UART1_TX_PIN            PA9
 #define UART1_RX_PIN            PA10
-@@ -62,32 +35,7 @@
+
+#define USE_UART2
 #define UART2_TX_PIN            PA2
 #define UART2_RX_PIN            PA3
 
@@ -83,24 +88,22 @@
 /*
  * I2C
  */
-// I2C
 #define USE_I2C
 #define USE_I2C_DEVICE_1
 #define I2C1_SCL                PB8
-@@ -96,44 +44,25 @@
+#define I2C1_SDA                PB9
+
 /*
  * Sensor drivers
  */
 
 // ICM42605/ICM42688P
-// IMU
 #define USE_IMU_ICM42605
 #define IMU_ICM42605_ALIGN      CW270_DEG
 #define ICM42605_CS_PIN         PA4
 #define ICM42605_SPI_BUS        BUS_SPI1
 
 // Baro
-// Baro - keep 1 small driver
 #define USE_BARO
 #define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_BMP280
@@ -128,19 +131,16 @@
 #define USE_SDCARD_SPI
 #define SDCARD_SPI_BUS          BUS_SPI3
 #define SDCARD_CS_PIN           PC14
-// OSD - remove MAX7456, keep MSP DisplayPort for DJI
-#define USE_MSP_DISPLAYPORT
 
 #define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
-// SD Card - disable to save space
-//#define USE_SDCARD
-//#define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
 
-// RX
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_CRSF
 #define SERIALRX_UART           SERIAL_PORT_USART1
-@@ -144,31 +73,23 @@
+
+// *************** ADC ***************************
+#define USE_ADC
+#define ADC_INSTANCE                ADC1
 #define ADC1_DMA_STREAM             DMA2_Stream0
 #define ADC_CHANNEL_1_PIN           PC0
 #define ADC_CHANNEL_2_PIN           PC1
@@ -154,15 +154,10 @@
 // *************** LED2812 ************************
 #define USE_LED_STRIP
 #define WS2811_PIN                      PA8
-// Remove LED strip to save space
-//#define USE_LED_STRIP
 
 // ***************  OTHERS *************************
 #define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TELEMETRY | FEATURE_BLACKBOX | FEATURE_AIRMODE)
 #define CURRENT_METER_SCALE   195
-// Features - remove telemetry except CRSF, remove blackbox
-#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_CURRENT_METER | FEATURE_VBAT)
-#define CURRENT_METER_SCALE     195
 
 #define USE_DSHOT
 #define USE_ESC_SENSOR
@@ -174,13 +169,7 @@
 #define TARGET_IO_PORTD         (BIT(2))
 
 #define MAX_PWM_OUTPUT_PORTS       11
-#define MAX_PWM_OUTPUT_PORTS    11
-
-#define USE_QUAD_MIXER 1
-#define USE_CUSTOM_MIXER 1
-@@ -177,4 +98,4 @@
 
 #define USE_PINIO
 #define USE_PINIOBOX
 #define PINIO1_PIN                  PC13
-#define PINIO1_PIN              PC13
